18c8aa56d09e5713070e20e5950b82caf7c2009c
jpinyot <pep.pinyot@gmail.com>
2020-10-27 09:59:18 +0100

    Solve problem when moving files

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp ./.my_vim_config/ ~/
+cp -r .my_vim_config/ ~/
 
-cp ./.vimrc ~/
+cp -r .vimrc ~/
